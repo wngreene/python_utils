45e9a8d277e5c6987c43d839aa18530a1eab708d
W. Nicholas Greene <wng@csail.mit.edu>
2014-11-28 18:38:51 -0500

    Add hatmap and veemap.

--- Geometry.py
+++ Geometry.py
@@ -19,9 +19,9 @@ class Quaternion(object):
         self.y = y
         self.z = z
         self.w = w
-        
+
         self.normalize()
-                
+
     def vec(self):
         return np.array([self.x, self.y, self.z, self.w])
 
@@ -30,19 +30,19 @@ class Quaternion(object):
         self.y = np.sin(angle/2)*axis[1]
         self.z = np.sin(angle/2)*axis[2]
         self.w = np.cos(angle/2)
-        
+
         self.normalize()
-        
+
         return self
-        
+
     def setFromRotationMatrix(self, mat):
-        
+
         # Copied from Eigen.
         # This algorithm comes from  "Quaternion Calculus and Fast Animation",
         # Ken Shoemake, 1987 SIGGRAPH course notes
-        
+
         assert mat.shape == (3, 3)
-        
+
         t = np.trace(mat)
         if t > 0:
             t = np.sqrt(t + 1.0)
@@ -59,10 +59,10 @@ class Quaternion(object):
                 i = 2
             j = (i+1) % 3
             k = (j+1) % 3
-            
+
             t = np.sqrt(mat[i, i] - mat[j, j] - mat[k, k] + 1.0)
-            
-            if i == 0:                
+
+            if i == 0:
                 self.x = 0.5 * t
             elif i == 1:
                 self.y = 0.5 * t
@@ -70,19 +70,19 @@ class Quaternion(object):
                 self.z = 0.5 * t
             elif i == 3:
                 self.w = 0.5 * t
-                        
+
             t = 0.5 / t
-                    
+
             self.w = (mat[k, j] - mat[j, k])*t
-            
-            if j == 0:                    
+
+            if j == 0:
                 self.x = (mat[j, i] + mat[i, j])*t
             elif j == 1:
                 self.y = (mat[j, i] + mat[i, j])*t
             elif j == 2:
                 self.z = (mat[j, i] + mat[i, j])*t
-                    
-            if k == 0:                
+
+            if k == 0:
                 self.x = (mat[k, i] + mat[i, k])*t
             elif k == 1:
                 self.y = (mat[k, i] + mat[i, k])*t
@@ -92,22 +92,22 @@ class Quaternion(object):
         self.normalize()
 
         return self
-        
+
     def norm(self):
         return np.sqrt(self.x**2 + self.y**2 + self.z**2 + self.w**2)
 
     def normalize(self):
         n = self.norm()
-        
+
         self.x /= n
         self.y /= n
         self.z /= n
         self.w /= n
-        
+
         return self
-        
+
     def getRotationMatrix(self):
-        # Copied from Eigen.  
+        # Copied from Eigen.
         tx  = 2.0*self.x
         ty  = 2.0*self.y
         tz  = 2.0*self.z
@@ -120,7 +120,7 @@ class Quaternion(object):
         tyy = ty*self.y
         tyz = tz*self.y
         tzz = tz*self.z
-        
+
         R = np.zeros((3, 3))
         R[0, 0] = 1.0-(tyy+tzz)
         R[0, 1] = txy-twz
@@ -131,9 +131,9 @@ class Quaternion(object):
         R[2, 0] = txz-twy
         R[2, 1] = tyz+twx
         R[2, 2] = 1.0-(txx+tyy)
-        
+
         return R
-    
+
     def slerp(self, t, other):
         '''
         Spherical linearl interpolation (from Eigen).
@@ -144,7 +144,7 @@ class Quaternion(object):
         thresh = float(1.0)  - np.spacing(1.0)
         d = np.dot(vec0, vec1)
         abs_d = np.abs(d)
-        
+
         scale0 = 0
         scale1 = 0
 
@@ -157,19 +157,53 @@ class Quaternion(object):
 
             scale0 = np.sin((1.0 - t ) * theta)/sin_theta;
             scale1 = np.sin(( t * theta))/sin_theta;
-            
+
         if d < 0:
             scale1 = -scale1
 
         new_vec =  scale0 * vec0 + scale1 * vec1
-        
+
         q = Quaternion(new_vec[0], new_vec[1], new_vec[2], new_vec[3])
 
         return q.normalize()
 
     def __str__(self):
         return "[%.17f, %.17f, %.17f, %.17f]" %(self.x, self.y, self.z, self.w)
-        
+
+
+def veemap(A):
+    """Vee-map
+
+    Extracts an R3 vector from an so3 (Lie Algebra) skew-symmetric matrix.
+    """
+    assert(A.shape == (3, 3))
+
+    ret = np.zeros(3)
+    ret[0] = A[2, 1]
+    ret[1] = A[0, 2]
+    ret[2] = A[1, 0]
+
+    return ret
+
+
+def hatmap(w):
+    """Hat-map
+
+    Maps an R3 vector to an so3 (Lie Algebra) skew-symmetric matrix.
+    """
+    assert(len(w) == 3)
+
+    ret = np.zeros((3, 3))
+    ret[1, 0] = w[2]
+    ret[2, 0] = -w[1]
+    ret[0, 1] = -w[2]
+    ret[2, 1] = w[0]
+    ret[0, 2] = w[1]
+    ret[1, 2] = -w[0]
+
+    return ret
+
+
 def skewSym(w):
     """
     Convert a 3 vector to a skew-symmetric matrix.
@@ -185,10 +219,10 @@ def skewSym(w):
     ret[1, 2] = -w[0]
 
     return ret
-    
+
 def so3LieToMat(w):
     """
-    Converts Lie algebra in SO3 to rotation matrix using 
+    Converts Lie algebra in SO3 to rotation matrix using
     exponential map (i.e. Rodrigues formula).
     http://ethaneade.com/latex2html/lie/node16.html
     """
@@ -208,8 +242,8 @@ def so3LieToMat(w):
     # Rodrigues formula.
     R = np.eye(3)
     R += A*wx + B*np.dot(wx, wx)
-    
-    return R    
+
+    return R
 
 def se3LieToRotTrans3(w, u):
     """
@@ -222,7 +256,7 @@ def se3LieToRotTrans3(w, u):
     assert(len(w) == 3)
     assert(len(u) == 3)
 
-    # First get rotation matrix.    
+    # First get rotation matrix.
     theta = np.linalg.norm(w)
 
     if theta > 1e-3:
@@ -257,7 +291,7 @@ def se3LieToMat4(w, u):
     exponential map.
     See http://ethaneade.com/latex2html/lie/node16.html
     """
-    
+
     assert(len(w) == 3)
     assert(len(u) == 3)
 
@@ -272,7 +306,7 @@ def se3LieToMat4(w, u):
 
 def rotTrans3ToMat4(R, t):
     """
-    Convert a 3D rotation matrix R and translation t to 
+    Convert a 3D rotation matrix R and translation t to
     a homogeneous transformation matrix.
     """
 
@@ -285,5 +319,3 @@ def rotTrans3ToMat4(R, t):
     T[3, 3] = 1.0
 
     return T
-
-    
