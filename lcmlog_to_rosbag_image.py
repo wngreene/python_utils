6de7af317875416941cab4cd62bc8760ff0f183e
W. Nicholas Greene <wngreene@mit.edu>
2015-01-21 19:51:12 -0500

    Add support for Fomat7/ROI stuff.

--- lcmlog_to_rosbag_image.py
+++ lcmlog_to_rosbag_image.py
@@ -23,6 +23,15 @@ def main():
     parser.add_argument('--right_camera_yml',
                         help='Image calibration YAML file from ROS calibrator',
                         action='append', dest='yml_files')
+
+    roi_parser = parser.add_argument_group("Format7/ROI", "Format7/ROI options needed when dealing with non-standard video modes.")
+    roi_parser.add_argument('--binning_x', default=1, type=int, dest='binning_x', help='Image binning factor.')
+    roi_parser.add_argument('--binning_y', default=1, type=int, dest='binning_y', help='Image binning factor.')
+    roi_parser.add_argument('--x_offset', default=0, type=int, dest='x_offset', help="ROI x offset (in UNBINNED pixels)")
+    roi_parser.add_argument('--y_offset', default=0, type=int, dest='y_offset', help="ROI y offset (in UNBINNED pixels)")
+    roi_parser.add_argument('--width', default=640, type=int, dest='width', help="ROI width (in UNBINNED pixels)")
+    roi_parser.add_argument('--height', default=480, type=int, dest='height', help="ROI height (in UNBINNED pixels)")
+    roi_parser.add_argument('--do_rectify', default=False, type=bool, dest='do_rectify', help="Do rectification when querying ROI.")
     args = parser.parse_args()
 
     args.lcm_channels.append(args.left_img_channel)
@@ -71,8 +80,8 @@ def main():
                     # Fill in camera info
                     camera_info = CameraInfo()
                     camera_info.header = ros_msg.header
-                    camera_info.height = lcm_msg.height
-                    camera_info.width = lcm_msg.width
+                    camera_info.height = y['image_width']
+                    camera_info.width = y['image_height']
 
                     if y["distortion_model"] != "plumb_bob":
                         print "Encountered non-supported distorion model %s. Skipping..." % y["distortion_model"]
@@ -83,9 +92,13 @@ def main():
                     camera_info.K = y["camera_matrix"]['data']
                     camera_info.R = y["rectification_matrix"]['data']
                     camera_info.P = y["projection_matrix"]['data']
-                    camera_info.binning_x = 0
-                    camera_info.binning_y = 0
-                    # camera_info.roi = sensor_msgs.msg.RegionOfInterest() 
+                    camera_info.binning_x = args.binning_x
+                    camera_info.binning_y = args.binning_y
+                    camera_info.roi.x_offset = args.x_offset
+                    camera_info.roi.y_offset = args.y_offset
+                    camera_info.roi.height = args.height
+                    camera_info.roi.width = args.width
+                    camera_info.roi.do_rectify = args.do_rectify
 
                     bag.write("/camera/" + l + "/image_raw/compressed", ros_msg)
                     bag.write("/camera/" + l + "/camera_info", camera_info)
